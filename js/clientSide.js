cefb0c69d85caca521cf5036ca860b7502b7e86f
Gabriel Viernes <viernesgabriel@gmail.com>
2023-10-02 18:50:27 -0500

    added omdb and reddit key

--- js/clientSide.js
+++ js/clientSide.js
@@ -1,5 +1,7 @@
 // ROTTEN TOMATOES KEY: emsqjacpmmqtms79muj8v6xm
 // ROTTEN TOMATOES SECRET: Qt7Fp4QJQG
+// OMDB KEY:  4efa80bc
+// REDDIT KEY(?): 2_oloNHhJx4G-Hsz6bIH5Q
 
 // async function logMovies() {
 //     const response = await fetch("https://api.github.com/users/victor");
