5e2b18406e5cc029e2ba48e1173ca8aef62b0ecf
Gabriel Viernes <viernesgabriel@gmail.com>
2023-10-02 18:47:21 -0500

    rotten tomatoes key and secret added to clientSide.js

--- js/clientSide.js
+++ js/clientSide.js
@@ -1,8 +1,13 @@
-async function logMovies() {
-    const response = await fetch("https://api.github.com/users/victor");
-    const user = await response.json();
-    return user
-  }
+// ROTTEN TOMATOES KEY: emsqjacpmmqtms79muj8v6xm
+// ROTTEN TOMATOES SECRET: Qt7Fp4QJQG
 
-const victor = logMovies()
-victor.then((value)=> console.log(value.html_url))
+// async function logMovies() {
+//     const response = await fetch("https://api.github.com/users/victor");
+//     const user = await response.json();
+//     return user
+//   }
+
+// const victor = logMovies()
+// victor.then((value)=> console.log(value.html_url))
+
+
