df4897b43b15080e01a1be746fe7c98579b9ae16
Negatseg <tseganehnegash@gmail.com>
2023-10-02 19:36:43 -0500

    add the fetch call function in java script

--- js/clientSide.js
+++ js/clientSide.js
@@ -2,18 +2,41 @@
 // ROTTEN TOMATOES SECRET: Qt7Fp4QJQG
 // OMDB KEY:  4efa80bc
 // REDDIT KEY(?): n4brKxZAfsAOHMyzmUlhpw
+const apiKey = '4efa80bc';
+const movieTitle = 'The Shawshank Redemption';
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieTitle)}`;
+
+console.log(apiUrl);
 
 // REDDIT SECRET: 	YF7RwwTWTPa8eUc6smx-WRMIbOxyuw
 // REDDIT USERNAME: movieLookup
 // REDDIT PASSWORD: keyboard
 
-// async function logMovies() {
-//     const response = await fetch("https://api.github.com/users/victor");
-//     const user = await response.json();
-//     return user
-//   }
+const logMoviesFunction = async () => {
+    const response = await fetch('apiUrl');
+     const user = await response.json();
+        console.log(user)
+     //return user
+   }
+logMoviesFunction()
 
-// const victor = logMovies()
-// victor.then((value)=> console.log(value.html_url))
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Handle the data received from the OMDB API
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch
+    console.error('Fetch error:', error);
+  });
+
+
 
 
