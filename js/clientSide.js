98af267837a481fd9dbd48ae54509c60c0eb5efe
Gabriel Viernes <viernesgabriel@gmail.com>
2023-10-02 19:08:32 -0500

    correct Reddit key and secret added

--- js/clientSide.js
+++ js/clientSide.js
@@ -1,7 +1,11 @@
 // ROTTEN TOMATOES KEY: emsqjacpmmqtms79muj8v6xm
 // ROTTEN TOMATOES SECRET: Qt7Fp4QJQG
 // OMDB KEY:  4efa80bc
-// REDDIT KEY(?): 2_oloNHhJx4G-Hsz6bIH5Q
+// REDDIT KEY(?): n4brKxZAfsAOHMyzmUlhpw
+
+// REDDIT SECRET: 	YF7RwwTWTPa8eUc6smx-WRMIbOxyuw
+// REDDIT USERNAME: movieLookup
+// REDDIT PASSWORD: keyboard
 
 // async function logMovies() {
 //     const response = await fetch("https://api.github.com/users/victor");
