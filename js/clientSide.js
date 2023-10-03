584120b7a82c6bcad4eae5c2b0c991891282981b
Gabriel Viernes <viernesgabriel@gmail.com>
2023-10-02 20:16:20 -0500

    made getMoveData accept user input and changed name to getObmbData

--- js/clientSide.js
+++ js/clientSide.js
@@ -2,18 +2,30 @@
 // ROTTEN TOMATOES SECRET: Qt7Fp4QJQG
 // OMDB KEY:  4efa80bc
 // REDDIT KEY(?): n4brKxZAfsAOHMyzmUlhpw
-const apiKey = '4efa80bc';
-const movieTitle = 'The Shawshank Redemption';
-const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieTitle)}`;
-
-console.log(apiUrl);
 
 // REDDIT SECRET: 	YF7RwwTWTPa8eUc6smx-WRMIbOxyuw
 // REDDIT USERNAME: movieLookup
 // REDDIT PASSWORD: keyboard
 
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Handle the data received from the OMDB API
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     // Handle any errors that occurred during the fetch
+//     console.error('Fetch error:', error);
+//   });
 
-async function fetchMovieData() {
+// encodeURIComponent replaces non english characters with escape sequences that can be read by APIs
+async function getOmdbData(movieTitle) {
+    const apiUrl = `http://www.omdbapi.com/?apikey=4efa80bc&t=${encodeURIComponent(movieTitle)}`
     try {
       const response = await fetch(apiUrl);
   
@@ -28,28 +40,21 @@ async function fetchMovieData() {
       // Handle any errors that occurred during the fetch
       console.error('Fetch error:', error);
     }
-  }
-  
-  // Call the async function to fetch movie data
-  fetchMovieData();
+    return data;
+}
 
-
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+async function getRedditAPI(input) {
+    let url = `https://api.reddit.com/r/movies/search/?q=${encodeURIComponent(input)}&restrict_sr=1`
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Reddit network response failed');
+        }
+        const data = await response.json();
+    } catch (error) {
+        console.error('Reddit fetch error:' + error)
     }
-    return response.json();
-  })
-  .then(data => {
-    // Handle the data received from the OMDB API
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('Fetch error:', error);
-  });
-
-
+    return data;
+}
 
 
